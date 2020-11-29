138bd99e58481e3d7ec8f9e0cdc86605f91b42f8
Roxanne Nasraty <rsnasraty@gmail.com>
2020-11-28 21:12:34 -0600

    Added kristne spencer responsive square layout to home

---
+++ src/components/home.js
@@ -0,0 +1,18 @@
+import './home.css';
+
+function Home() {
+return (
+    <div className="home">
+
+    
+<div className="box box1"></div>
+<div className="box box2"></div>
+<div className="box box3"></div>
+<div className="box box4"></div>
+<div className="box box5"></div>
+<div className="box box6"></div>
+</div>
+);
+}
+
+export default Home;
