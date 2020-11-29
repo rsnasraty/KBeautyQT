93029befd462da07889b8e0a555700f7dc9c6ee5
Roxanne Nasraty <rsnasraty@gmail.com>
2020-11-28 22:43:15 -0600

    Corrected css

--- src/components/home.js
+++ src/components/home.js
@@ -2,12 +2,17 @@ import './home.css';
 
 function Home() {
 return (
+    <div className="home_header">
+    Welcome!
     <div className="home">
-<div className="box box1"></div>
+    
+
+<div className="box box1"></div> 
 <div className="box box2"></div>
 <div className="box box3"></div>
 <div className="box box4"></div>
 <div className="box box5"></div>
+</div>
 </div>
 );
 }
