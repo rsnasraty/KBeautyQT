d4e7e2d9dc1b5b50b02da1579aa79159bcfdb042
Roxanne Nasraty <rsnasraty@gmail.com>
2020-11-30 00:31:46 -0600

    Cleaned up home

--- src/components/home.js
+++ src/components/home.js
@@ -3,19 +3,19 @@ import React from "react";
 import { Navbar} from "./navbar"
 
 function Home() {
-  return (
+return (
     <div className="home_header">
 
     <Navbar />
-      <div className="home">
+    <div className="home">
         <div className="box box1"></div>
         <div className="box box2"></div>
         <div className="box box3"></div>
         <div className="box box4"></div>
         <div className="box box5"></div>
-      </div>
     </div>
-  );
+    </div>
+);
 }
 
 export default Home;
