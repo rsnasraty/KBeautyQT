c6ff5899016e3c233f4d1d1dcfed6c06e504573d
Roxanne Nasraty <rsnasraty@gmail.com>
2021-01-23 14:50:21 -0600

    Added products preview to home

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 import { Home } from './pages/home'
+import { Routes } from './pages/routes'
+import { BrowserRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>,
+    <Routes />
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
